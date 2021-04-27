1e92521150aeafb8a533e0f6f815d952fa9a6bc6
somiryu <somiryu@gmail.com>
2021-04-27 18:28:31 -0500

    artree first push

--- app/admin/fotos.rb
+++ app/admin/fotos.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Foto do
   permit_params :referencia, :artistum_id, :nombre, :descripcion,
   	:precio_base, :prioridad, :activo, :color, :destacado, :destacado_temporada, 
-  	:linea_id, :stock, :numero_actual, :pineado
+  	:linea_id, :stock, :numero_actual, :pineado, :sala_id
 end
