f8df68f2ea56e0f410fb27222f5d4d47a3125285
Steven Rodriguez <stevenfreemann8@gmail.com>
2021-05-28 10:03:57 -0500

    ignore

--- app/javascript/components/views/infoExclusive.js
+++ app/javascript/components/views/infoExclusive.js
@@ -2,7 +2,16 @@ import React from 'react'
 import Carrousel from '../Carousel'
 import Title from '../Title'
 import SectionInfo from '../SectionInfo'
+import infoMaterials from '../../assets/static/images/infoMaterials.png'
+import img from '../../assets/static/images/javier-vanegas.png'
 const colors = ['var(--backGray)', 'white', 'var(--backPurple)', 'var(--backCream)']
+const texto = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor neque placeat culpa eos accusantium itaque molestiae cupiditate, ipsa non rem sint facilis blanditiis impedit consequuntur nisi. Totam earum distinctio odio!'
+
+const info = [
+  {title: 'Javier Vanegas',text:texto,textoBtn:'Ver todos los artistas', img:img},
+  {title: 'MATERIALES',text:texto,textoBtn:'Ver todos los materiales', img:infoMaterials}
+]
+
 export default function infoExclusive() {
     return (
         <>
