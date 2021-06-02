37870b276b6289ea3519de1d0971b7d4ed308738
Steven Rodriguez <stevenfreemann8@gmail.com>
2021-06-02 11:06:08 -0500

    Carousel and others

--- app/javascript/components/Carousel.js
+++ app/javascript/components/Carousel.js
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import Home1 from '../assets/static/images/home1.png'
 import Home2 from '../assets/static/images/home2.png'
+import Banner1 from '../assets/static/images/banner1.jpeg'
+import Banner2 from '../assets/static/images/banner2.jpeg'
+import Banner3 from '../assets/static/images/banner3.jpeg'
+import Banner4 from '../assets/static/images/banner4.jpeg'
+import Banner5 from '../assets/static/images/banner5.jpg'
 import Left from '../assets/static/buttons/left-arrow.png'
 import Right from '../assets/static/buttons/right-arrow.png'
 import ContentCarousel from './ContentCarousel'
 // import Indicators from './Indicators'
-const imgDumm = [Home1, Home2]
 const contentCarousel = [
     {
         Title: null,
         text: null,
-        img: Home1
+        img: Banner1
     },
     {
         Title: 'TÍTULO',
         text: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-        img: Home2
+        img: Banner2
+    },
+    {
+        Title: null,
+        text: null,
+        img: Banner3
+    },
+    {
+        Title: 'TÍTULO',
+        text: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        img: Banner4
+    },
+    {
+        Title: 'TÍTULO',
+        text: 'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        img: Banner5
     }
 ]
 export default function Carrousel() {
@@ -24,12 +43,13 @@ export default function Carrousel() {
         // 0 is right
         // 1 is left
         let slides = document.getElementsByClassName('carousel')[0]
-        if ((dir === 1 && index > 0) || (dir === 0 && index < imgDumm.length - 1)) {
+        if ((dir === 1 && index > 0) || (dir === 0 && index < contentCarousel.length - 1)) {
             slides.classList.add('carousel--slide')
         }
         setTimeout(() => {
             if (dir === 1 && index > 0) { setIndex(index - 1); slides.classList.replace('carousel--slide', 'carousel--show') }
-            else if (dir === 0 && index < imgDumm.length - 1) { setIndex(index + 1); slides.classList.replace('carousel--slide', 'carousel--show') }
+            else if (dir === 0 && index < contentCarousel.length - 1) { setIndex(index + 1); slides.classList.replace('carousel--slide', 'carousel--show') }
+            else if(dir === 0 && index===contentCarousel.length-1) setIndex(0)
             setTimeout(() => {
                 slides.classList.remove('carousel--show')
             }, 300)
