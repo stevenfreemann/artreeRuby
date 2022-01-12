438f53787bea8f8f7b0d30ad15c8f5e9ec6fa981
msantamaria86 <msantamaria86@gmail.com>
2022-01-12 14:52:54 -0500

    fix before_action

--- app/controllers/wompi_controller.rb
+++ app/controllers/wompi_controller.rb
@@ -1,5 +1,5 @@
 class WompiController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def index
     render json: @data
