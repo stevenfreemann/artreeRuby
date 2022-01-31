79d7ce9512a89c049188cc8f06cf41112f3349a1
msantamaria86 <msantamaria86@gmail.com>
2022-01-31 10:31:42 -0500

    fix user name

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ if !AdminUser.first
 end
 
 if !User.first
-	User.create!(email: 'prueba1@gmail.com', password: '123456', password_confirmation: '123456')
+	User.create!(email: 'prueba1@gmail.com', password: '123456', password_confirmation: '123456', name: 'Test test')
 end
 
 # if !WishItem.first
