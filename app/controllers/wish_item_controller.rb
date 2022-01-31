4ce63309db492d8476f752dffe3649529f6b7ec7
msantamaria86 <msantamaria86@gmail.com>
2022-01-31 18:16:02 -0500

    antes del restore de transacciones

--- app/controllers/wish_item_controller.rb
+++ app/controllers/wish_item_controller.rb
@@ -1,5 +1,9 @@
 class WishItemController < ApplicationController
   def index
-    @products = current_user.wish_items 
+    if current_user
+      @products = current_user.wish_items 
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
