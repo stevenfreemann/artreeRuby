ac6665b24bd410ad0e8a7d11734418754ad3046f
msantamaria86 <msantamaria86@gmail.com>
2022-03-10 10:28:14 -0500

    cambio email usuario

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -90,7 +90,7 @@ class TransactionsController < ApplicationController
     transaction = Transaction.find_by(ref_number: json["reference_sale"])
     transaction.status = json["response_message_pol"]
     if transaction.status == "APPROVED"
-      AdminMailer.with( email: current_user.email , id:transaction.payment_id, cost: transaction.total_cost).confirmation_mail.deliver_later
+      AdminMailer.with( email: json["email_buyer"] , id:transaction.payment_id, cost: transaction.total_cost).confirmation_mail.deliver_later
     end
     transaction.last_4 = json["cc_number"][-4..-1]
     transaction.payment_id = json["transaction_id"]
