6a7a7ebef7a6502eac713073b3048918e9169bc8
msantamaria86 <msantamaria86@gmail.com>
2022-03-14 18:43:29 -0500

    comment mailer production

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,3 @@
-# Load the Rails application.
-require_relative 'application'
-
-# Initialize the Rails application.
-Rails.application.initialize!
-
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
   :password => ENV["SENDGRID"], # This is the secret sendgrid API key which was issued during API key creation
@@ -13,3 +7,9 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
