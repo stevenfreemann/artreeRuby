b290792b51c11fd77b717c327667c5e2df626418
msantamaria86 <msantamaria86@gmail.com>
2022-03-14 19:06:47 -0500

    testing sendgrid_actionmailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,15 @@
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => ENV["SENDGRID"], # This is the secret sendgrid API key which was issued during API key creation
-  :domain => 'https://artree-shop.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+#   :password => ENV["SENDGRID"], # This is the secret sendgrid API key which was issued during API key creation
+#   :domain => 'artree-shop.herokuapp.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
