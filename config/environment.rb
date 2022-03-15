cdf350c4b1c577d13bb995ea1cefc79849ee874c
msantamaria86 <msantamaria86@gmail.com>
2022-03-15 08:48:02 -0500

    smtp6

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.smtp_settings = {
+  user_name: 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  password: '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', # This is the secret sendgrid API key which was issued during API key creation
+  domain: 'artree-shop.herokuapp.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: "plain",
+  enable_starttls_auto: true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,10 +59,13 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "artree_production"
-
-
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = SMTP_SETTINGS
+  # config.action_mailer.default_url_options = { host: "https://artree-shop.herokuapp.com" }
+  # config.action_mailer.sendgrid_actionmailer_settings = {
+  #   api_key: ENV["SENDGRID"],
+  #   raise_delivery_errors: true
+  # }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
